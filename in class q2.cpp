a7374788ed253526e4ad74aa80c5025e4bdc3e8e
mohammedabdelhmid <mohammedabdelhmid58@gmail.com>
2026-05-15 00:15:04 +0200

    Add files via upload

---
+++ in class q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+float add(float a, float b, float c, float d)
+{
+    return a + b + c + d;
+}
+
+int main()
+{
+    cout << add(1.2, 2.5, 5.4, 6.5);
+
+    return 0;
+}
